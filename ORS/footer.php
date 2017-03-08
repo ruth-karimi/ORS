a0fc1d5943bca7372271c4e2c6cbec5b78327b81
ruth-karimi <perisruthnyakio@gmail.com>
2017-03-08 14:00:44 +0000

    Footer

---
+++ ORS/footer.php
@@ -0,0 +1,27 @@
+ <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">About ORS</h5>
+                <p class="grey-text text-lighten-4">ORS is an NGO that seeks to help the orphans in Rwanda by providing them with food clothings and ensuring they
+                have access to quality education. We have different sponsors for our children and volunteers for our programs. Donate above to help a kid or volunteer
+                to touch a heart.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Our Social Media Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">FaceBook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            <div class="container">
+            © <?php echo date("Y")?> Alrights reserved
+            <a class="grey-text text-lighten-4 right" href="#!">ORS</a>
+            </div>
+          </div>
+        </footer>
